eb022fd21cbb690b81cbab3781e4565d384b569c
AnnaBerman <55157299+AnnaBerman@users.noreply.github.com>
2019-12-04 22:50:47 -0800

    styling + delete task + mark task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,29 +5,58 @@ function App() {
   const [task, setTask] = useState("");
 
   function addTask() {
-    setTasks([
-      ...tasks,
-      {
-        id: tasks.length,
-        value: task
-      }
-    ]);
-    setTask("");
+    if (task && task.length > 0) {
+      setTasks([
+        ...tasks,
+        {
+          id: tasks.length,
+          value: task
+        }
+      ]);
+      setTask("");
+    }
+  }
+
+  function removeTask(event) {
+    var array = [...tasks];
+    array.splice(event.target.attributes["index"].value, 1);
+    setTasks([...array]);
   }
 
   function handleChange(event) {
     setTask(event.target.value);
   }
 
+  function handleKeyPress(target) {
+    if (target.charCode === 13) {
+      addTask();
+    }
+  }
+
+  function markTask(event) {
+    event.target.className === "done"
+      ? (event.target.className = "")
+      : (event.target.className = "done");
+  }
+
   return (
     <div className="container">
-      <input type="text" onChange={handleChange} value={task} />
+      <h1>todo list</h1>
+      <input
+        onKeyPress={handleKeyPress}
+        type="text"
+        onChange={handleChange}
+        value={task}
+      />
       <button onClick={addTask}>Add Task</button>
-      <ol>
-        {tasks.map(task => (
-          <li key={task.id}>{task.value}</li>
+      <ul>
+        {tasks.map((task, index) => (
+          <li onClick={markTask} key={task.id}>
+            <span>{task.value}</span>
+            <span index={index} onClick={removeTask} className="close" />
+          </li>
         ))}
-      </ol>
+      </ul>
     </div>
   );
 }
